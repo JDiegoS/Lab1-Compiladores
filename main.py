0cb7ab2632ff2a34df5d1708ea7bbbd9031bbecc
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2022-07-31 04:09:59 -0600

    MyVisitor

--- main.py
+++ main.py
@@ -12,12 +12,27 @@ def main():
     tree = parser.program()
 
     # Errores semanticos
-    visitor = ParserVisitor()
-    output = visitor.visit(tree)
-    print(output)
-
+    visitor = MyVisitor()
+    visitor.visit(tree)
+    
     #Arbol
     os.system('grun Parser program test.cl -gui -tokens')
+
+class MyVisitor(ParserVisitor):
+    def visitProgram(self, ctx):
+        print("program")
+        return self.visitChildren(ctx)
+    
+    def visitFeature(self, ctx:ParserParser.FeatureContext):
+        print("feature")
+
+        return self.visitChildren(ctx)
+
+    def visitStringExpr(self, ctx:ParserParser.StringExprContext):
+        if (len(ctx.getText()) > 10):
+            print("ERROR: Longitud de string excedida\n\tLinea [%s:%s] %s" % (ctx.start.line, ctx.start.column, ctx.getText()))
+
+        return self.visitChildren(ctx)
 
 if __name__ == "__main__":
     main()
